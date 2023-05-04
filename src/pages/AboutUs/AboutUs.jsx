3e28e7ae453bd0bd01779dcee758be546ad97f8b
vaidehi <vaidehiramilla16@gmail.com>
2023-05-04 23:11:42 +0530

    done

--- src/pages/AboutUs/AboutUs.jsx
+++ src/pages/AboutUs/AboutUs.jsx
@@ -1,81 +1,77 @@
-
-import Styles from "./AboutUs.module.css";
 import Navbar from "../../component/Navbar/Navbar"
+import styles from './AboutUs.module.css'
 import Footer from "../../component/Footer/Footer"
+
+
 export default function AboutUs() {
+
   return (
-<>
-<Navbar/>
-<div id="about" className={Styles.Main}>
-      <div className={Styles.AboutUs}>
-        <div className={Styles.Video1}>
+    <div id='aboutus'>
+      <Navbar />
+      <div className={styles.aboutus}>
+        <div className={styles.left}>
+
           <iframe
-            width="360"
-            height="515"
-            src="https://www.youtube.com/embed/f6t9TQqdUzg"
+            width="650"
+            height="500"
+            src="https:www.youtube.com/embed/f6t9TQqdUzg"
             title="YouTube video player"
-            // frameborder="0"
-            // allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-            // allowfullscreen={true}
-            allowFullScreen={true}
-          ></iframe>
-        </div>
-        <div className={Styles.ContainerHeading}>
-          <h1>About Us</h1>
-          <h3>
-            Through our 7-day programme we offer you some useful life questions
-            to think about. We also offer you some answers on these questions
-            but we encourage you to learn more about it yourself. You can read
-            it all at once but we advise you to read and think about your life
-            for the next 7 days. It may become the most valuable week of your
-            life! It can be very valuable in your life. So take some time and
-            invest one week in your life and future!
-          </h3>
-          <button className={Styles.button}>Learn More</button>
-        </div>
-      </div>
-
-      <div className={Styles.AboutUs}>
-        <div>
-          <h1>Why Choose Us ?</h1>
-          <ul className={Styles.listContainer}>
-            <li>
-              <strong className={Styles.strong}>
-                Consultation with expert
-              </strong>
-              <h4>
-                Plus Fitness franchisee Jacob Awad first started his journey
-                with the brand in 2013 after purchasing Plus Fitness Seven Hills
-                from a previous owner. In 2017 Jacob then went on to purchase
-                his second location, Plus Fitness Northmead.
-              </h4>
-            </li>
-            <li>
-              <strong className={Styles.strong}> workout faculitis</strong>
-              <h4>
-                Plus Fitness franchisee Jacob Awad first started his journey
-                with the brand in 2013 after purchasing Plus Fitness Seven Hills
-                from a previous owner. In 2017 Jacob then went on to purchase
-                his second location, Plus Fitness Northmead.
-              </h4>
-            </li>
-          </ul>
-        </div>
-
-        <div className={Styles.Video2}>
-          <iframe
-            width="360"
-            height="515"
-            src="https://www.youtube.com/embed/cllK0z8BrlQ"
-            title="YouTube video player 😈"
-            // frameborder="0"
-            // allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
             allowfullscreen={true}
-          ></iframe>
+            allowFullScreen={true}
+          >
+          </iframe>
+
+        </div>
+
+        <div className={styles.right}>
+
+          <h1> About <span style={{ color: 'red', fontSize: "3rem" }}>Us </span> </h1>
+          <p className={styles.p}>
+            Latest fitness equipment. 10,000 sq.ft. of gym workout space. A brand new gym.
+            Dedicated personal training and CrossFit Studio. Internationally certified personal trainers. We are the champions of fitness.
+            We are Waves Gym. If you prefer a more personalized approach, our certified personal trainers are available to
+            create custom workout plans tailored to your specific needs and goals.
+            We have no qualms in calling ourselves the best gym in Andheri West. Not only do you get assistance at every step and point,
+            but you also get personalized and customized workout and dietary programs.
+          </p>
+          <br />
+          <button className={styles.btn} onClick={() => alert('Clicked for AboutUs Section')}>Learn More</button>
         </div>
       </div>
+
+
+      <div className={styles.aboutus}>
+
+        <div className={styles.right}>
+
+          <h1>Why to <span style={{ color: 'red', fontSize: "3rem" }}>Choose</span>  Us</h1><br />
+          <p className={styles.p}>
+            We believe that fitness is not just about working out, it's about living a healthy and balanced lifestyle. That's why we offer a range
+            of amenities to help you relax and rejuvenate after your workout. Our locker rooms and showers are spacious and well-equipped,
+            and our saunas are the perfect place
+            to unwind and destress. We also have a juice bar where you can refuel with delicious and healthy drinks and snacks.
+            Our club is more than just a gym; it's a community of like-minded individuals who are passionate about living a healthy and
+            active lifestyle. We provide a welcoming
+            and inclusive environment where everyone is encouraged to unleash their potential and achieve their fitness goals.
+          </p>
+        </div>
+
+        <div className={styles.left}>
+
+          <iframe
+            width="650"
+            height="500"
+            src="https:www.youtube.com/embed/cllK0z8BrlQ"
+            title="YouTube video player 😈"
+            allowfullscreen={true}
+          >
+          </iframe>
+        </div>
+      </div>
+
+      <Footer />
     </div>
-    <Footer/>
-</>
-  );
+
+  )
 }
+
--- src/pages/Pricing/Pricing.jsx
+++ src/pages/Pricing/Pricing.jsx
@@ -1,82 +1,76 @@
-
-// import { Link } from "react-router-dom";
-import styles from "./Pricing.module.css";
+import Style from "./Pricing.module.css";
 import Navbar from "../../component/Navbar/Navbar"
 import Footer from "../../component/Footer/Footer"
+import { useNavigate } from "react-router-dom";
 export default function Pricing() {
+
+  const navigate = useNavigate()
+
   return (
 <>
 <Navbar/>
-<div className={styles.pricing_mainBackground}>
-      <div className={styles.pricing_container}>
-        <div className={styles.pass_part}>
-          <div className={`${styles.pass_box} ${styles.left_box}`}>
-            <div>
-              <p>Day Pass</p>
-              <h1>$20</h1>
-              <span>/pass</span>
-            </div>
-            <p>-- 1 Day Pass</p>
-            <p>-- Free Gym Access</p>
-            <p>-- 24 Hour Access</p>
-            {/* <Link to="/joinUs">
-              <button className={styles.btn}> Buy</button>
-            </Link> */}
-            <button className={styles.btn}> Buy</button>
-          </div>
+ 
+<div id='pricing'>
 
-          <div className={`${styles.pass_box} ${styles.right_box}`}>
-            <div>
-              <p>Month to Month</p>
-              <h1>$90</h1>
-              <span>/month</span>
-            </div>
-            <p>-- $99 Joining Fee</p>
-            <p>-- No Contract</p>
-            <p>-- Free Gym Access</p>
-            <p>-- 1 Group Class Included</p>
-            <p>-- 24 Hour Access</p>
-            {/* <Link to="/joinUs">
-              <button className={styles.btnn}> Buy</button>
-            </Link> */}
-            <button className={styles.btnn}> Buy</button>
-          </div>
-        </div>
+<div className={Style.container}>
+  
+  <div className={Style.card1}>
+    <span className={Style.s1}> Day Pass </span> <br />
+    <span className={Style.s2}> $  </span>  
+    <span className={Style.s3}> 20 </span>   <br />
+    <span className={Style.s4}> /pass </span> <br />
+    <p className={Style.para}>
+      ☑️ 1 Day Pass <br /> <br />
+      ☑️ Free Gym Access <br /> <br />
+      ☑️ 24 Hours Access <br />
+    </p>
 
-        <div className={styles.content_part}>
-          <h4>
-            <span>Member</span>ship
-          </h4>
+    
+    <button className={Style.btn} onClick={() => navigate('/login')}> GET STARTED </button>
+  </div>
+  
 
-          <h2>From Punch Pass to Monthly or Annual</h2>
+  <div className={Style.card2}>
+    <span className={Style.s5}> Month to Month </span>  <br />
+    <span className={Style.s6}> $  </span>  
+    <span className={Style.s7}> 90 </span>   <br />
+    <span className={Style.s8}> /month </span> <br />
+    <p className={Style.p}>
+    ☑️ $99 Joining Free  <br /> <br />
+    ☑️ No Contact  <br /> <br />
+    ☑️ Free Gym Access   <br /> <br />
+    ☑️ 1 Group Class Include  <br /> <br />
+    ☑️ 24 Hours Access <br />
+    </p>
+    <button className={Style.btns} onClick={() => navigate('/login')}> GET STARTED </button>
+  </div>
+  
 
-          <p>
-          At Gym base we offer wide range of membership with opposite to suit every budget.
+  <div className={Style.content}>
+
+    <p  className={Style.heading}>Membership</p>
+
+    <p className={Style.p1}>From Punch Pass to Monthly or Annual </p>
+
+    <p className={Style.p2}>At Gym base we offer wide range of membership with opposite to suit every budget.
       Everything from one Day Pass. Punch Pass to monthly or annual prepaid membership. 
-      What's more. We won't tie you in to a long term contract giving you greater flexibility.          </p>
+      What's more. We won't tie you in to a long term contract giving you greater flexibility.
+    </p>
 
-          <h3>Each Plan included</h3>
+      <p className={Style.p3}>Each Plan included </p> <br />
 
-          <ul>
-            <li>
-              <span>--</span> The best equipment global brand
-            </li>
-            <li>
-              <span>--</span> The gym is open 24 hours a day, 7 days a week
-            </li>
-            <li>
-              <span>--</span> Two safe payment methods
-            </li>
-            <li>
-              <span>--</span> Group fitness classes in the price of the
-              subscription
-            </li>
-            <li>
-              <span>--</span> No long-term contract, period
-            </li>
-          </ul>
-        </div>
+      <p className={Style.p4}>
+      ➡️ Best equiement global brands. <br /> 
+      ➡️ The gym opens 24 hours a day , 7 days a week  <br />
+      ➡️ Two safe payment methods. <br />
+      ➡️ Group fitness classes in price of subscription. <br />
+      ➡️ No long-term contract , period.
+      </p>
       </div>
+</div>
+
+
+      {/* <Footer /> */}
     </div>
     <Footer/>
 </>
